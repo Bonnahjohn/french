034fc2ff6a3e6ef0cb68d4bea0d3e206f61b87d1
John Bonnah <bonnahpoku@gmail.com>
2023-05-03 22:27:56 +0800

    splash screen demo is setup for the app

---
+++ lib/screens/splas.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: ((context) => Home())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          child: Text('me dofo'),
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("GeeksForGeeks")),
+      body: Center(
+          child: Text(
+        "Home page",
+        textScaleFactor: 2,
+      )),
+    );
+  }
+}
