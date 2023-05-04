233b99a63b7d2a7663b5be68c01838b01e725d6c
John Bonnah <bonnahpoku@gmail.com>
2023-05-05 00:05:32 +0800

    informal letter profile is built

--- lib/screens/media.dart
+++ lib/screens/media.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 
 class Media extends StatefulWidget {
-  const Media({super.key});
+  const Media({Key? key}) : super(key: key);
 
   @override
   State<Media> createState() => _MediaState();
@@ -15,57 +13,79 @@ class _MediaState extends State<Media> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(indicatorColor: Colors.white, tabs: [
+        appBar: AppBar(
+          bottom: const TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
               Tab(
                 child: Text('Formal Letter'),
               ),
               Tab(
                 child: Text('Informal Letter'),
               ),
-            ]),
-            title: Text('Sample images of letter'),
+            ],
           ),
-          body: TabBarView(children: [Sider(), Text('data')])),
+          title: const Text('Sample images of letter'),
+        ),
+        body: const TabBarView(
+          children: [
+            Sider(),
+            Text('data'),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class Sider extends StatelessWidget {
-  const Sider({super.key});
+  const Sider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 1000,
-      width: 100,
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            'To view or zoom the picture,please kindly double tap on the image.',
-            style: TextStyle(fontSize: 20),
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        SizedBox(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'To view or zoom the picture, double tap on the image.',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Divider(
+                color: Colors.green,
+                height: 30,
+                thickness: 10,
+              ),
+              Image.asset(
+                'assets/images/F1.png',
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Divider(
+                color: Colors.green,
+                height: 30,
+                thickness: 10,
+              ),
+              Image.asset(
+                'assets/images/F1.png',
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 70,
+              )
+            ],
           ),
-          Expanded(
-            flex: 3,
-            child: Image.asset(
-              'assets/images/letter.png',
-              width: 600,
-              height: 400,
-            ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Image.asset(
-              'assets/images/letter.png',
-              width: 600,
-              height: 400,
-            ),
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
